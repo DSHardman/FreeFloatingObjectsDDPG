e74336a0d572a834ce90c4b9bc25883b28e1f646
David <dshardman@outlook.com>
2021-02-26 09:07:26 +0000

    Updated test for 12 parameters

--- TestAgent_IROS.m
+++ TestAgent_IROS.m
@@ -45,7 +45,7 @@ n = 50;
 mcritic = saved_agent.getCritic();
 
 states = 2*rand(n,3) - ones(n,3);
-actions = zeros(n,11);
+actions = zeros(n,12);
 rewards = zeros(n,1);
 for i = 1:n
     actions(i,:) = saved_agent.getAction(states(i,:)).';
@@ -53,16 +53,18 @@ for i = 1:n
 end
 
 ur5variables = ["xamp" "xfreq" "xphase" "yamp" "yfreq" "yphase" "zamp"...
-    "zfreq" "depth" "anglex" "angley"];
+    "zfreq" "depth" "anglex" "angley" "decay"];
 figure();
-for i = 1:11
+for i = 1:12
     subplot(4,3,i);
     plot(actions(:,i));
     ylim([-1 1]);
     title(ur5variables(i));
 end
+%{
 subplot(4,3,12);
 plot(rewards);
 ylim([-2 3]);
 hold on;
-title("Reward");
+%}
+%title("Reward");
